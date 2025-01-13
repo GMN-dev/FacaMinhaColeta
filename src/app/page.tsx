28ed9d88782f5723b25875df65259d89eb81b708
GMN-dev <g.magalhaes.dev@gmail.com>
2025-01-13 10:29:30 -0300

    added css to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-import Image from "next/image";
+import "./globals.css"
 
 export default function Home() {
   return (
     <main>
-      <div className="container">
+      <div className="container-top">
         <h1 className="title-header">Relatório de coleta de equipamentos - Brasília.</h1>
         <button className="newAssetBtn" type="button">Adicionar equipamento</button>
       </div>
       <section className="main-section">
         <section className="section-container">
-          <h3>Pendente coleta</h3>
-          <div className="assets-container">
+          <h3>Pendente</h3>
+          <div className="items">
           </div>
-          <div className="items"></div>
+        </section>
+        <section className="section-container">
+          <h3>Agendado</h3>
+          <div className="items">
+          </div>
         </section>
         <section className="section-container">
           <h3>Coletados</h3>
