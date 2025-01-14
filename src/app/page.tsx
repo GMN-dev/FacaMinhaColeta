a7296eae780e4adeb68228d006a5d7174ee67cdf
GMN-dev <g.magalhaes.dev@gmail.com>
2025-01-14 15:51:02 -0300

    button with modal function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+"use client"
+
 import "./globals.css"
 import Asset from "@/components/Asset/Asset";
 import Modal from "@/components/Modal/Modal";
+import { ModalContext } from "@/context/AppContext";
+import { useContext } from "react";
+
+
 export default function Home() {
+  const {isEnable, setIsEnable}: any = useContext(ModalContext);
+  
+  function showModal(): void{
+    setIsEnable(true);
+  }
+
   return (
     <main>
       <Modal></Modal>
       <div className="container-top">
         <h1 className="title-header">Relatório de coleta de equipamentos - Brasília.</h1>
-        <button className="newAssetBtn" type="button">Adicionar equipamento</button>
+        <button className="newAssetBtn" onClick={showModal} type="button">Adicionar equipamento</button>
       </div>
       <section className="main-section">
         <section className="section-container">
