731255c04b7ffd9e45e93ca0532e0efb83993bc8
GMN-dev <g.magalhaes.dev@gmail.com>
2025-01-15 09:46:00 -0300

    first form model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ import { useContext } from "react";
 
 
 export default function Home() {
-  const {isEnable, setIsEnable}: any = useContext(ModalContext);
+  const {setIsEnable}: any = useContext(ModalContext);
   
-  function showModal(): void{
+  function enableModal(): void{
     setIsEnable(true);
   }
 
@@ -19,7 +19,7 @@ export default function Home() {
       <Modal></Modal>
       <div className="container-top">
         <h1 className="title-header">Relatório de coleta de equipamentos - Brasília.</h1>
-        <button className="newAssetBtn" onClick={showModal} type="button">Adicionar equipamento</button>
+        <button className="newAssetBtn" onClick={enableModal} type="button">Adicionar equipamento</button>
       </div>
       <section className="main-section">
         <section className="section-container">
